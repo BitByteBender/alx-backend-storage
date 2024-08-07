0d46b418bf8c451ab6f514e4ec6edcbf9f27cb49
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-07 20:20:54 +0100

    Safe Div

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -1,16 +1,16 @@
 -- Script that creates a function SafeDiv that divides (and returns) the first by the second
 -- number or returns 0 if the second number is equal to 0
 DROP FUNCTION IF EXISTS SafeDiv;
+
 DELIMITER //
-CREATE FUNCTION SafeDiv (a INTEGER, b INTEGER) RETURNS DECIMAL(10,2)
+CREATE FUNCTION SafeDiv (a INT, b INT) RETURNS DECIMAL(10,2)
 BEGIN
 	DECLARE calc DECIMAL(10,2) DEFAULT 0;
 
-	IF b = 0 THEN
-		RETURN 0;
-	ELSE
+	IF b != 0 THEN
 		SET calc = a / b;
-		RETURN calc;
 	END IF;
+
+	RETURN calc;
 END //
 DELIMITER ;
