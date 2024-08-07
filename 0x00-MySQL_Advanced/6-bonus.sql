891e4f0bed6d63d0e3c8fa080c884efe8dfcadf2
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-07 18:04:47 +0100

    Retest

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -10,14 +10,22 @@ CREATE PROCEDURE AddBonus(
 BEGIN
 	DECLARE proc_project_id INTEGER DEFAULT 0;
 	DECLARE user_exists INTEGER DEFAULT 0;
+	DECLARE project_count INTEGER DEFAULT 0;
 
 	SELECT COUNT(*) INTO user_exists FROM users WHERE id = proc_user_id;
 
-	SELECT id INTO proc_project_id FROM projects WHERE name = proc_project_name;
+	IF user_exists = 0 THEN
+		SIGNAL SQLSTATE '45000'
+		SET MESSAGE_TEXT = 'Warning! UserID Does not exist.';
+	END IF;
 
-	IF proc_project_id IS NULL THEN
-		INSERT INTO projects (name) VALUES (proc_project_name);
-		SET proc_project_id = LAST_INSERT_ID();
+	SELECT COUNT(id) INTO project_count FROM projects WHERE name = proc_project_name;
+
+	IF project_count = 0 THEN
+		INSERT INTO projects(name) VALUES (proc_project_name);
+		SET proc_project_id= LAST_INSERT_ID();
+	ELSE
+		SELECT id INTO proc_project_id FROM projects WHERE name = proc_project_name;
 	END IF;
 
 	INSERT INTO corrections (user_id, project_id , score) VALUES (proc_user_id, proc_project_id, proc_score);
