a547d59b5669977f051ddd58dc8a45e610c3b3c3
Soufiane Sadgali <ssadgali2@gmail.com>
2024-08-08 20:05:04 +0100

    No table for a meeting

---
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -0,0 +1,4 @@
+-- Script that creates a view that lists all students and have a score under 80
+DROP VIEW IF EXISTS need_meeting;
+CREATE VIEW need_meeting AS SELECT name FROM students
+WHERE score < 80 AND (last_meeting IS NULL OR last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH));
